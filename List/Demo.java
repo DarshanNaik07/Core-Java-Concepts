9fb8a90786b2ae8cc2436827aea263c06e849975
Darshan Naik <147927156+DarshanNaik07@users.noreply.github.com>
2026-06-08 12:16:41 +0530

    Initial commit

---
+++ List/Demo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Demo{
+    public static void main(String[] args){
+        List<Integer> nums = new ArrayList<Integer>();
+        nums.add(6);
+        nums.add(7);
+        nums.add(26);
+        nums.add(86);
+        nums.add(62);
+
+        System.out.println(nums.size());
+
+        // for(Integer i: nums){
+        //         System.out.println(i);
+        // }
+        // System.out.println(nums);
+    }
+}
