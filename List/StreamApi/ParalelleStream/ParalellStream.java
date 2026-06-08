213169b0ecb1e50de13ff98c302963c48b58c469
Darshan Naik <147927156+DarshanNaik07@users.noreply.github.com>
2026-06-09 02:02:25 +0530

    I have checked which one is fast api(Stream or parallelStream). Then i realised parallel is faster than the stream. I have used inbuilt sum() and maptoInt() method for calculations.

---
+++ List/StreamApi/ParalelleStream/ParalellStream.java
@@ -0,0 +1,36 @@
+package StreamApi.ParalelleStream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ParalellStream {
+    public static void main(String[] args) {
+
+        int size = 10_000;
+        List<Integer> nums = new ArrayList<>(size);
+
+        Random ran = new Random();
+        for(int i=1; i<=size; i++){
+            nums.add(ran.nextInt(100));
+        }
+
+        long startSeq = System.currentTimeMillis();
+        int Sum1 = nums.stream()
+                   .map(i -> i*2)
+                   .mapToInt(i->i)
+                   .sum();
+        long endSeq = System.currentTimeMillis();
+
+        long startpara = System.currentTimeMillis();
+        int Sum2 = nums.parallelStream()
+                   .map(i -> i*2)
+                   .mapToInt(i->i)
+                   .sum();
+        long endPara = System.currentTimeMillis();
+
+        System.out.println(Sum1 + " " + Sum2);
+        System.out.println("seq" + (startSeq - endSeq));
+        System.out.println("para" + (startpara - endPara));
+    }
+}
