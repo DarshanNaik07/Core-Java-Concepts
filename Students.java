b10d1c8143c445dd80be6152723e9d160987dafa
Darshan Naik <147927156+DarshanNaik07@users.noreply.github.com>
2026-06-09 00:14:39 +0530

    Added lambda Expression to student sorting program

--- Students.java
+++ Students.java
@@ -1,3 +1,45 @@
+// import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.Comparator;
+// import java.util.List;
+// public class Students {
+
+//     String name;
+//     int age;
+
+//     public Students(String name, int age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     public String toString() {
+//         return "Students [name=" + name + ", age=" + age + "]";
+//     }
+
+//     public static void main(String[] args) {
+
+//         Comparator<Students> ascendingorder = new Comparator<>(){
+//             public int compare(Students a, Students b){
+//                 if(a.age > b.age){
+//                     return 1;
+//                 }
+//                 else{
+//                         return -1;
+//                     }
+//             }
+//         };
+//         List<Students> studs = new ArrayList<>();
+//         studs.add(new Students("ganesh", 85));
+//         studs.add(new Students("alice", 92));
+//         studs.add(new Students("bob", 78));
+//         studs.add(new Students("charlie", 95));
+//         studs.add(new Students("david", 88));
+
+//         Collections.sort(studs, ascendingorder);
+//         System.out.println("After sorting: " + studs);
+//     }
+// }
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -18,16 +60,7 @@ public class Students {
 
     public static void main(String[] args) {
 
-        Comparator<Students> ascendingorder = new Comparator<>(){
-            public int compare(Students a, Students b){
-                if(a.age > b.age){
-                    return 1;
-                }
-                else{
-                        return -1;
-                    }
-            }
-        };
+        Comparator<Students> ascendingorder = (a, b) -> a.age > b.age ? 1 : -1;
         List<Students> studs = new ArrayList<>();
         studs.add(new Students("ganesh", 85));
         studs.add(new Students("alice", 92));
@@ -36,6 +69,6 @@ public class Students {
         studs.add(new Students("david", 88));
 
         Collections.sort(studs, ascendingorder);
-        System.out.println("After sorting: " + studs);
-    }
+        System.out.println("After sorting: " +studs);
+}
 }
